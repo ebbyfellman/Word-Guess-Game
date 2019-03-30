79c81954aa02e2eaaeca78c15be63048e9bc4f1c
efellman <ebbyfellman@icloud.com>
2019-03-30 09:48:10 -0600

    Update to javascript to handle wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,30 @@
+function test (){
+
+//Create array of cosmic word.
 var cosmicWords = ["planet", "galaxy", "sun", "star", "comet", "astroid", "meteor", "eclipse", "equinox", "extraterrestrial", "moon", "nebula", "supernova", "satellite"];
 
+//Choose random word from array.
 var index = Math.floor(Math.random() * cosmicWords.length)
-
 var word = cosmicWords[index];
+
+
 console.log(word)
 var wordLength = word.length;
 var currentWord = [];
+
+//Create blank word
 for (i = 0; i < wordLength; i++) {
     currentWord[i] = "_";
 }
-var winsText = document.getElementById("wins");
+
+
+var wins = document.getElementById("wins");
 var current = document.getElementById("current");
 var guesses = document.getElementById("guesses");
 var letters = document.getElementById("letters"); 
 
+
+//Write current word to page
 for (i = 0; i < currentWord.length; i++) {
     current.innerHTML += currentWord[i];
 }
@@ -25,15 +36,17 @@ for (i = 0; i < currentWord.length; i++) {
 
 var guessRemaining = 20;
 var lettersGuessed = [];
+var answer = "n";
 
-document.onkeyup = function (event) {
 
     // This function is run whenever the user presses a key.
-
+    document.onkeyup = function (event){
 
     // Determines which key was pressed.
     var userGuess = event.key;
+    
 
+    //Compare user guess to each letter in array
     for (j = 0; j < currentWord.length; j++) {
         if (word.charAt(j) === userGuess) {
             currentWord[j] = userGuess;
@@ -41,6 +54,7 @@ document.onkeyup = function (event) {
 
     }
 
+    //Keep track of letters guessed and add to array.
     if (!lettersGuessed.includes(userGuess.toUpperCase())) {
         lettersGuessed.push(userGuess.toUpperCase());
     }
@@ -50,21 +64,26 @@ document.onkeyup = function (event) {
         current.innerHTML += currentWord[i];
     }
 
+
+    //Keep track of guesses remaining
     guessRemaining--;
     guesses.textContent = guessRemaining;
 
     letters.textContent = lettersGuessed;
 
+
+    //If user guesses word, print you win.  
+    //If they run out of guesses, print you lose.
     if (current.innerHTML == word) {
-        winsText.innerHTML = "You win!"
-        setTimeout(location.reload(), 150000);
+        wins.innerHTML = "You WIN!!!";
     }
 
     if (guessRemaining < 0){
-        lose.innerHTML = "You LOSE!!!"
-        setTimeout(location.reload(), 150000);
+        lose.innerHTML = "You LOSE!!!";
     }
-
-
+}
 
 }
+
+
+
